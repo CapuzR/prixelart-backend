928b3cb8baeb83a054919eeef15acbae740dfbd7
Edward Guerrero <iamwar2070@gmail.com>
2023-01-02 17:34:50 -0400

    feat: added service and controllers for order and consumer creation and modification

--- consumer/consumerRoutes.js
+++ consumer/consumerRoutes.js
@@ -1,15 +1,34 @@
-'use strict'
+"use strict";
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const consumerControllers = require('./consumerControllers');
-const adminAuthServices = require('../admin/adminServices/adminAuthServices');
+const consumerControllers = require("./consumerControllers");
+const adminAuthServices = require("../admin/adminServices/adminAuthServices");
 
-// Incapaz de crear el usuario.
-router.post('/consumer/create', adminAuthServices.ensureAuthenticated, consumerControllers.createConsumer);
-router.post('/consumer/read', adminAuthServices.ensureAuthenticated, consumerControllers.readConsumer);
-router.post('/consumer/read-by-query', adminAuthServices.ensureAuthenticated, consumerControllers.readConsumerByQuery);
-router.post('/consumer/read-all', adminAuthServices.ensureAuthenticated, consumerControllers.readAllConsumers);
-router.post('/consumer/update', adminAuthServices.ensureAuthenticated, consumerControllers.updateConsumer);
+router.post(
+  "/consumer/create",
+  //  adminAuthServices.ensureAuthenticated,
+  consumerControllers.createConsumer
+);
+router.post(
+  "/consumer/read",
+  adminAuthServices.ensureAuthenticated,
+  consumerControllers.readConsumer
+);
+router.post(
+  "/consumer/read-by-query",
+  adminAuthServices.ensureAuthenticated,
+  consumerControllers.readConsumerByQuery
+);
+router.post(
+  "/consumer/read-all",
+  adminAuthServices.ensureAuthenticated,
+  consumerControllers.readAllConsumers
+);
+router.post(
+  "/consumer/update",
+  adminAuthServices.ensureAuthenticated,
+  consumerControllers.updateConsumer
+);
 
 module.exports = router;
--- order/orderModel.js
+++ order/orderModel.js
@@ -1,31 +1,34 @@
-'use strict'
+"use strict";
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const OrderSchema = Schema({
-    orderId: {type: Number, required: true},
-    active: {type: String, required: true},
-    consumerId: {type: String, required: true},
-    orderType: {type: String, required: true},
-    createdOn: {type: Date, required: true},
-    createdBy: {type: Object, required: true},
-    subtotal: {type: Number, required: true},
-    tax: {type: Number, required: true},
-    total: {type: Number, required: true}, //Arreglo
-    shippingAddress: {type: String, required: true}, //Arreglo,
-    billingAddress: {type: String, required: true},
-    shippingPhone: {type: String, required: false},
-    internalShippingMethod: {type: String, required: false}, //(Yalo, DH, etc)
-    domesticShippingMethod: {type: String, required: false}, //(Tealca, Zoom, etc)
-    internationalShippingMethod: {type: String, required: false}, //(DHL, FedEx, Particular, etc)
-    generalProductionStatus: {type: String, required: false},
-    paymentStatus: {type: String, required: false}, //(Por pagar, Pagado parcialmente, Pagado)
-    shippingStatus: {type: String, required: false}, //(No entregado, Entregado)
-    observations: {type: String, required: false},
-    isSaleByPrixer: {type: Boolean, required: false}
-    // artProductionStatus (Solicitado, En diseño, En impresión, Listo)
-    // baseProductionStatus (Solicitado, En producción, Listo)
+  orderId: { type: Number, required: true },
+  // active: {type: String, required: true},
+  // consumerId: {type: String, required: true},
+  orderType: { type: String, required: true },
+  createdOn: { type: Date, required: true },
+  createdBy: { type: Object, required: true },
+  subtotal: { type: Number, required: true },
+  tax: { type: Number, required: true },
+  total: { type: Number, required: true }, //Arreglo
+  shippingAddress: { type: String, required: true }, //Arreglo,
+  billingAddress: { type: String, required: true },
+  requests: { type: Array, required: true },
+  orderPaymentMethod: { type: String, required: true },
+  status: { type: String, required: true },
+  shippingPhone: { type: String, required: false },
+  internalShippingMethod: { type: String, required: false }, //(Yalo, DH, etc)
+  domesticShippingMethod: { type: String, required: false }, //(Tealca, Zoom, etc)
+  internationalShippingMethod: { type: String, required: false }, //(DHL, FedEx, Particular, etc)
+  generalProductionStatus: { type: String, required: false },
+  paymentStatus: { type: String, required: false }, //(Por pagar, Pagado parcialmente, Pagado)
+  shippingStatus: { type: String, required: false }, //(No entregado, Entregado)
+  observations: { type: String, required: false },
+  isSaleByPrixer: { type: Boolean, required: false },
+  // artProductionStatus (Solicitado, En diseño, En impresión, Listo)
+  // baseProductionStatus (Solicitado, En producción, Listo)
 });
 
-module.exports = mongoose.model('Order', OrderSchema, "order");
+module.exports = mongoose.model("Order", OrderSchema, "order");
