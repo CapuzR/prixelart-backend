33070447c6d5b850c88798608b054e1aece97939
Edward Guerrero <iamwar2070@gmail.com>
2023-02-02 00:55:55 -0400

    fix: fixing admin issues

--- preferences/preferencesRoutes.js
+++ preferences/preferencesRoutes.js
@@ -10,22 +10,39 @@ const {
 } = require("./preferencesController");
 const express = require("express");
 const preferencesRoutes = express.Router();
+const adminAuthServices = require("../admin/adminServices/adminAuthServices");
 
 preferencesRoutes.get("/carousel", readAllImagesCarousel);
 preferencesRoutes.get("/carousel/:id", readImageCarousel);
 preferencesRoutes.post(
   "/carousel",
-  upload.fields([{name: "bannerImagesDesktop", maxCount: 1}, {name: "bannerImagesMobile", maxCount: 1}]),
+  adminAuthServices.ensureAuthenticated,
+  upload.fields([
+    { name: "bannerImagesDesktop", maxCount: 1 },
+    { name: "bannerImagesMobile", maxCount: 1 },
+  ]),
   createImageCarousel
 );
 preferencesRoutes.put(
   "/carousel/:id",
-  upload.fields([{name: "bannerImagesDesktop", maxCount: 1}, {name: "bannerImagesMobile", maxCount: 1}]),
+  adminAuthServices.ensureAuthenticated,
+  upload.fields([
+    { name: "bannerImagesDesktop", maxCount: 1 },
+    { name: "bannerImagesMobile", maxCount: 1 },
+  ]),
   updateImageCarousel
 );
-preferencesRoutes.delete("/carousel/:id", deleteImageCarousel);
+preferencesRoutes.delete(
+  "/carousel/:id",
+  adminAuthServices.ensureAuthenticated,
+  deleteImageCarousel
+);
 
 preferencesRoutes.get("/termsAndConditions/read", readTermsAndConditions);
-preferencesRoutes.put("/termsAndConditions/update", updateTermsAndConditions);
+preferencesRoutes.put(
+  "/termsAndConditions/update",
+  adminAuthServices.ensureAuthenticated,
+  updateTermsAndConditions
+);
 
 module.exports = preferencesRoutes;
