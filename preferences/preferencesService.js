25de341e9bfb8978a4c4c46860790c26f2b22fcf
Edward Guerrero <iamwar2070@gmail.com>
2023-02-24 18:18:56 -0400

    chore: reduce the dollar function

--- preferences/preferencesService.js
+++ preferences/preferencesService.js
@@ -2,23 +2,23 @@ const { dollarValue } = require("./preferencesModel");
 
 const updateDollarValue = async (dollar) => {
   try {
-    const result = await dollarValue.find();
-    if (result[0] !== undefined) {
-      const updating = await dollarValue.findOne({ _id: result[0]._id });
-      updating.dollarValue = dollar;
-      await updating.save();
-      return {
-        success: true,
-        newDollar: updating,
-      };
-    } else {
-      let newDollar = await new dollarValue({
-        dollarValue: dollar,
-      }).save();
-      return {
-        res: { success: true, dollarId: newDollar._id },
-      };
-    }
+    // const result = await dollarValue.find();
+    // if (result[0] !== undefined) {
+    //   const updating = await dollarValue.findOne({ _id: result[0]._id });
+    //   updating.dollarValue = dollar;
+    //   await updating.save();
+    //   return {
+    //     success: true,
+    //     newDollar: updating,
+    //   };
+    // } else {
+    let newDollar = await new dollarValue({
+      dollarValue: dollar,
+    }).save();
+    return {
+      res: { success: true, dollarId: newDollar._id },
+    };
+    // }
   } catch (e) {
     console.log(e);
     return "Error al actualizar el valor.";
