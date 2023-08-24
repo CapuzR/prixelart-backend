2e94fb05bf49e51759548bb7c3f0ae98e0e90536
Edward Guerrero <iamwar2070@gmail.com>
2023-08-24 11:30:16 -0400

    fix: unable checkPermissions in createMovement :facepalm:

--- movements/movementControllers.js
+++ movements/movementControllers.js
@@ -5,29 +5,29 @@ const adminAuthServices = require("../admin/adminServices/adminAuthServices");
 
 const createMovement = async (req, res) => {
   try {
-    let checkPermissions = await adminAuthServices.checkPermissions(
-      req.body.adminToken
-    );
-    if (checkPermissions.setPrixerBalance) {
-      const movement = {
-        _id: req.body._id,
-        createdOn: req.body.createdOn,
-        createdBy: req.body.createdBy,
-        destinatary: req.body.destinatary,
-        description: req.body.description,
-        type: req.body.type,
-        value: req.body.value,
-      };
-      const newMovement = await movementServices.createMovement(movement);
+    // let checkPermissions = await adminAuthServices.checkPermissions(
+    //   req.body.adminToken
+    // );
+    // if (checkPermissions.setPrixerBalance) {
+    const movement = {
+      _id: req.body._id,
+      createdOn: req.body.createdOn,
+      createdBy: req.body.createdBy,
+      destinatary: req.body.destinatary,
+      description: req.body.description,
+      type: req.body.type,
+      value: req.body.value,
+    };
+    const newMovement = await movementServices.createMovement(movement);
 
-      const updateBalance = await movementServices.updateBalance(movement);
-      res.send({ newMovement, updateBalance });
-    } else {
-      return res.send({
-        success: false,
-        message: "No tienes autorización para realizar esta acción.",
-      });
-    }
+    const updateBalance = await movementServices.updateBalance(movement);
+    res.send({ newMovement, updateBalance });
+    // } else {
+    //   return res.send({
+    //     success: false,
+    //     message: "No tienes autorización para realizar esta acción.",
+    //   });
+    // }
   } catch (err) {
     res.status(500).send(err);
   }
