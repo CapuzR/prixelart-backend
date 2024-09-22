632f10e3eccd12734f3ca79f807fdce80d2b1162
Edward Guerrero <iamwar2070@gmail.com>
2024-09-22 00:39:27 -0400

    feat: new route for delete Prixer's data

--- prixer/prixerRoutes.js
+++ prixer/prixerRoutes.js
@@ -1,65 +1,71 @@
-"use strict";
+"use strict"
 const { upload, upload2 } =
   // avatarUpload
-  require("./prixerControllers");
+  require("./prixerControllers")
 
-const express = require("express");
-const router = express.Router();
-const prixerMiddlewares = require("./prixerMiddlewares");
-const adminAuthServices = require("../admin/adminServices/adminAuthServices");
-const userMdw = require("../user/userMiddlewares");
-const prixerControllers = require("./prixerControllers");
-const prixerServices = require("./prixerServices");
+const express = require("express")
+const router = express.Router()
+const prixerMiddlewares = require("./prixerMiddlewares")
+const adminAuthServices = require("../admin/adminServices/adminAuthServices")
+const userMdw = require("../user/userMiddlewares")
+const prixerControllers = require("./prixerControllers")
+const prixerServices = require("./prixerServices")
 
 router.post(
   "/prixer-registration",
   userMdw.ensureAuthenticated,
   upload.single("avatar"),
   prixerControllers.createPrixer
-);
-router.post("/prixer/read", prixerControllers.readPrixer);
-router.get("/prixer/get/:id", prixerControllers.getPrixer);
-router.get("/prixer/getBio/:id", prixerControllers.getBio);
-router.get("/prixer/read-all", prixerControllers.readAllPrixers);
+)
+router.post("/prixer/read", prixerControllers.readPrixer)
+router.get("/prixer/get/:id", prixerControllers.getPrixer)
+router.get("/prixer/getBio/:id", prixerControllers.getBio)
+router.get("/prixer/read-all", prixerControllers.readAllPrixers)
 router.post(
   "/prixer/getOwnersAndPrixers",
   adminAuthServices.ensureAuthenticated,
   prixerControllers.getOwnersAndPrixers
-);
-router.get("/prixer/read-all-full", prixerControllers.readAllPrixersFull);
-router.get("/prixer/read-all-full-v2", prixerControllers.readAllPrixersFullv2);
+)
+router.get("/prixer/read-all-full", prixerControllers.readAllPrixersFull)
+router.get("/prixer/read-all-full-v2", prixerControllers.readAllPrixersFullv2)
 router.post(
   "/prixer/update",
   upload2.single("avatar"),
   // prixerMiddlewares.avatarUpload,
   userMdw.ensureAuthenticated,
   prixerControllers.updatePrixer
-);
+)
 router.put(
   "/prixer/updateBio/:id",
   userMdw.ensureAuthenticated,
   upload2.array("newBioImages", 4),
   prixerControllers.updateBio
-);
+)
 router.put(
   "/prixer/update-home/:id",
   adminAuthServices.ensureAuthenticated,
   prixerControllers.updateVisibility
-);
+)
 router.put(
   "/prixer/update-terms/:id",
   userMdw.ensureAuthenticated,
   prixerControllers.updateTermsAgree
-);
+)
 router.put(
   "/prixer/update-home/updateTermsAgree",
   adminAuthServices.ensureAuthenticated,
   prixerControllers.updateTermsAgreeGeneral
-);
+)
 router.put(
   "/prixers/addRole",
   adminAuthServices.ensureAuthenticated,
   prixerServices.addRole
-);
+)
 
-module.exports = router;
+router.put(
+  "/prixers/destroyPrixer",
+  adminAuthServices.ensureAuthenticated,
+  prixerControllers.destroyPrixer
+)
+
+module.exports = router
--- prixer/prixerServices.js
+++ prixer/prixerServices.js
@@ -1,173 +1,176 @@
-const Prixer = require("./prixerModel");
-const Organizations = require("../organizations/organizationModel");
-const User = require("../user/userModel");
-const userService = require("../user/userServices/userServices");
-const accountServices = require("../account/accountServices");
-const artServices = require("../art/artServices");
-const serviceServices = require("../serviceOfPrixers/serviceServices");
+const Prixer = require("./prixerModel")
+const Organizations = require("../organizations/organizationModel")
+const User = require("../user/userModel")
+const userService = require("../user/userServices/userServices")
+const accountServices = require("../account/accountServices")
+const artServices = require("../art/artServices")
+const serviceServices = require("../serviceOfPrixers/serviceServices")
+const Art = require("../art/artModel")
+const Service = require("../serviceOfPrixers/serviceModel")
+const Consumer = require("../consumer/consumerModel")
 
 //CRUD
 const createPrixer = async (prixerData) => {
-  prixerData.id = prixerData.userId;
-  const isPrixer = await readPrixer(prixerData);
+  prixerData.id = prixerData.userId
+  const isPrixer = await readPrixer(prixerData)
   try {
     if (isPrixer) {
       return {
         success: false,
         message: "Disculpa, este usuario ya está asignado a un Prixer",
-      };
+      }
     } else {
       return {
         success: true,
         prixerData: await new Prixer(prixerData).save(),
-      };
+      }
     }
   } catch (e) {
-    console.log(e);
+    console.log(e)
     return {
       success: false,
       message:
         e +
         "Disculpa. No se pudo cargar tus datos de Prixer, inténtalo de nuevo por favor.",
-    };
+    }
   }
-};
+}
 
 const mergePrixerAndUser = (readedPrixer, readedUser) => {
-  let prixer = {};
+  let prixer = {}
 
-  prixer["prixerId"] = readedPrixer.id;
-  prixer["username"] = readedUser.username;
-  prixer["firstName"] = readedUser.firstName;
-  prixer["lastName"] = readedUser.lastName;
-  prixer["email"] = readedUser?.email;
-  prixer["specialtyArt"] = readedPrixer?.specialtyArt;
-  prixer["description"] = readedPrixer?.description;
-  prixer["instagram"] = readedPrixer?.instagram;
-  prixer["facebook"] = readedPrixer?.facebook;
-  prixer["twitter"] = readedPrixer?.twitter;
-  prixer["dateOfBirth"] = readedPrixer?.dateOfBirth;
-  prixer["phone"] = readedPrixer?.phone;
-  prixer["country"] = readedPrixer?.country;
-  prixer["city"] = readedPrixer?.city;
-  prixer["avatar"] = readedPrixer?.avatar;
-  prixer["status"] = readedPrixer?.status;
-  prixer["termsAgree"] = readedPrixer?.termsAgree;
-  prixer["account"] = readedUser?.account || undefined;
-  prixer["role"] = readedUser?.role;
+  prixer["prixerId"] = readedPrixer.id
+  prixer["username"] = readedUser.username
+  prixer["firstName"] = readedUser.firstName
+  prixer["lastName"] = readedUser.lastName
+  prixer["email"] = readedUser?.email
+  prixer["specialtyArt"] = readedPrixer?.specialtyArt
+  prixer["description"] = readedPrixer?.description
+  prixer["instagram"] = readedPrixer?.instagram
+  prixer["facebook"] = readedPrixer?.facebook
+  prixer["twitter"] = readedPrixer?.twitter
+  prixer["dateOfBirth"] = readedPrixer?.dateOfBirth
+  prixer["phone"] = readedPrixer?.phone
+  prixer["country"] = readedPrixer?.country
+  prixer["city"] = readedPrixer?.city
+  prixer["avatar"] = readedPrixer?.avatar
+  prixer["status"] = readedPrixer?.status
+  prixer["termsAgree"] = readedPrixer?.termsAgree
+  prixer["account"] = readedUser?.account || undefined
+  prixer["role"] = readedUser?.role
 
-  return prixer;
-};
+  return prixer
+}
 
 const readPrixer = async (prixerData) => {
   if (prixerData === null) {
-    return null;
+    return null
   } else {
     let readedPrixer = await Prixer.findOne({
       username: prixerData.username,
-    }).exec();
+    }).exec()
     if (readedPrixer) {
-      const readedUser = await userService.readUserById({ id: prixerData._id });
-      const prixer = await mergePrixerAndUser(readedPrixer, readedUser);
-      return prixer;
+      const readedUser = await userService.readUserById({ id: prixerData._id })
+      const prixer = await mergePrixerAndUser(readedPrixer, readedUser)
+      return prixer
     }
 
-    return readedPrixer;
+    return readedPrixer
   }
-};
+}
 
 const readPrixerbyId = async (user) => {
-  let readedPrixer = await Prixer.findOne({ userId: user._id }).exec();
+  let readedPrixer = await Prixer.findOne({ userId: user._id }).exec()
   if (readedPrixer) {
-    const readedUser = await userService.readUserById({ id: user._id });
-    const prixer = await mergePrixerAndUser(readedPrixer, readedUser);
-    return prixer;
+    const readedUser = await userService.readUserById({ id: user._id })
+    const prixer = await mergePrixerAndUser(readedPrixer, readedUser)
+    return prixer
   }
 
-  return readedPrixer;
-};
+  return readedPrixer
+}
 
 const readBio = async (user) => {
-  let readedPrixer = await Prixer.findOne({ userId: user._id }).exec();
+  let readedPrixer = await Prixer.findOne({ userId: user._id }).exec()
   if (readedPrixer) {
-    const data = readedPrixer.bio;
-    return { data: data, success: true };
+    const data = readedPrixer.bio
+    return { data: data, success: true }
   }
-};
+}
 
 const readAllPrixers = async () => {
   try {
-    const readedPrixers = await Prixer.find({ status: true }).exec();
+    const readedPrixers = await Prixer.find({ status: true }).exec()
     if (readedPrixers) {
       const data = {
         info: "Todos los Prixers disponibles",
         prixers: readedPrixers,
-      };
-      return data;
+      }
+      return data
     } else {
       const data = {
         info: "No hay Prixers registrados",
         prixers: null,
-      };
-      return data;
+      }
+      return data
     }
   } catch (error) {
-    console.log(error);
-    return error;
+    console.log(error)
+    return error
   }
-};
+}
 
 const readAllPrixersFull = async () => {
   try {
-    const readedPrixers = await Prixer.find({}).exec();
-    let data = [];
+    const readedPrixers = await Prixer.find({}).exec()
+    let data = []
     if (readedPrixers) {
       data = await Promise.all(
         readedPrixers.map(async (readedPrixer) => {
           const readedUser = await userService.readUserById({
             id: readedPrixer.userId,
-          });
+          })
           if (
             readedUser &&
             (readedUser.role === "Prixer" || readedUser.role === undefined)
           ) {
-            const prixer = mergePrixerAndUser(readedPrixer, readedUser);
-            return prixer;
+            const prixer = mergePrixerAndUser(readedPrixer, readedUser)
+            return prixer
           }
         })
-      );
-      const filteredData = data.filter((element) => element !== undefined);
+      )
+      const filteredData = data.filter((element) => element !== undefined)
       const prixers = {
         info: "Prixers disponibles",
         prixers: filteredData,
-      };
-      return prixers;
+      }
+      return prixers
     } else {
       const data = {
         info: "No hay Prixers registrados",
         prixers: null,
-      };
-      return data;
+      }
+      return data
     }
   } catch (error) {
-    console.log(error);
-    return error;
+    console.log(error)
+    return error
   }
-};
+}
 
 const readAllPrixersFullv2 = async () => {
   try {
-    const readedPrixers = await Prixer.find({ status: true }).exec();
-    let data = [];
+    const readedPrixers = await Prixer.find({ status: true }).exec()
+    let data = []
     if (readedPrixers) {
       data = await Promise.all(
         readedPrixers.map(async (readedPrixer) => {
           const readedUser = await userService.readUserById({
             id: readedPrixer.userId,
-          });
+          })
           if (readedUser) {
-            const prixer = mergePrixerAndUser(readedPrixer, readedUser);
-            return prixer;
+            const prixer = mergePrixerAndUser(readedPrixer, readedUser)
+            return prixer
           } else {
             return {
               info:
@@ -175,100 +178,100 @@ const readAllPrixersFullv2 = async () => {
                 readedPrixer.username +
                 " no está asignado a un usuario.",
               prixers: null,
-            };
+            }
           }
         })
-      );
+      )
       const prixers = {
         info: "Prixers disponibles",
         prixers: data,
-      };
+      }
 
-      return prixers;
+      return prixers
     } else {
       const data = {
         info: "No hay Prixers registrados",
         prixers: null,
-      };
-      return data;
+      }
+      return data
     }
   } catch (error) {
-    console.log(error);
-    return error;
+    console.log(error)
+    return error
   }
-};
+}
 
 const getOwnersAndPrixers = async () => {
   try {
-    const readedOrgs = await Organizations.find({ status: true });
-    const readedPrixers = await Prixer.find({ status: true });
-    let users = [];
+    const readedOrgs = await Organizations.find({ status: true })
+    const readedPrixers = await Prixer.find({ status: true })
+    let users = []
     readedOrgs.map((org) => {
-      users.push(org.username);
-    });
+      users.push(org.username)
+    })
     readedPrixers.map((prixer) => {
-      users.push(prixer.username);
-    });
+      users.push(prixer.username)
+    })
     if (readedPrixers) {
       const data = {
         info: "Todos los Usuarios activos",
         users: users,
-      };
-      return data;
+      }
+      return data
     } else {
       const data = {
         info: "No hay Prixers registrados",
         prixers: null,
-      };
-      return data;
+      }
+      return data
     }
   } catch (error) {
-    console.log(error);
-    return error;
+    console.log(error)
+    return error
   }
-};
+}
 
 const updatePrixer = async (prixerData, userData) => {
   try {
-    const toUpdatePrixer = await Prixer.findOne({ userId: userData.id });
-    toUpdatePrixer.specialtyArt = prixerData.specialtyArt.split(",");
-    toUpdatePrixer.instagram = prixerData.instagram;
-    toUpdatePrixer.facebook = prixerData.facebook;
-    toUpdatePrixer.twitter = prixerData.twitter;
-    toUpdatePrixer.dateOfBirth = prixerData.dateOfBirth;
-    toUpdatePrixer.phone = prixerData.phone;
-    toUpdatePrixer.country = prixerData.country;
-    toUpdatePrixer.city = prixerData.city;
-    toUpdatePrixer.username = prixerData.username;
-    toUpdatePrixer.avatar = prixerData.avatar;
-    toUpdatePrixer.description = prixerData.description;
-    if (prixerData.specialty === "") toUpdatePrixer.specialty = [];
-    const updatedPrixer = await toUpdatePrixer.save();
+    const toUpdatePrixer = await Prixer.findOne({ userId: userData.id })
+    toUpdatePrixer.specialtyArt = prixerData.specialtyArt.split(",")
+    toUpdatePrixer.instagram = prixerData.instagram
+    toUpdatePrixer.facebook = prixerData.facebook
+    toUpdatePrixer.twitter = prixerData.twitter
+    toUpdatePrixer.dateOfBirth = prixerData.dateOfBirth
+    toUpdatePrixer.phone = prixerData.phone
+    toUpdatePrixer.country = prixerData.country
+    toUpdatePrixer.city = prixerData.city
+    toUpdatePrixer.username = prixerData.username
+    toUpdatePrixer.avatar = prixerData.avatar
+    toUpdatePrixer.description = prixerData.description
+    if (prixerData.specialty === "") toUpdatePrixer.specialty = []
+    const updatedPrixer = await toUpdatePrixer.save()
     if (!updatedPrixer) {
-      return console.log("Prixer update error: " + err);
+      return console.log("Prixer update error: " + err)
     }
-    const updatedUser = await userService.updateUser(userData);
-    const prixer = mergePrixerAndUser(updatedPrixer, updatedUser);
+    const updatedUser = await userService.updateUser(userData)
+    const prixer = mergePrixerAndUser(updatedPrixer, updatedUser)
 
-    return prixer;
+    return prixer
   } catch (e) {
-    console.log(e);
-    return e;
+    console.log(e)
+    return e
   }
-};
+}
 
 const updateVisibility = async (prixerData) => {
   try {
     const toUpdatePrixer = await Prixer.findOne({
       _id: prixerData.id,
-    });
-    let unableArts, unableServices;
+    })
+    let unableArts, unableServices
     // if (prixerData.status === false && typeof prixerData.account === "string") {
-    toUpdatePrixer.status = prixerData.status;
+    toUpdatePrixer.status = prixerData.status
     if (prixerData.status === false) {
-      unableArts = await artServices.unableArts(toUpdatePrixer.username);
+      unableArts = await artServices.unableArts(toUpdatePrixer.username)
 
-      unableServices = await serviceServices.unableServices(toUpdatePrixer.id);
+      unableServices = await serviceServices.unableServices(toUpdatePrixer.id)
     }
     // const deleteAccount = await accountServices.deleteAccount(
     //   prixerData.account
@@ -278,87 +281,116 @@ const updateVisibility = async (prixerData) => {
     // });
     // const updatedPrixer = await toUpdatePrixer.save();
     // } else {
-    const updatedPrixer = await toUpdatePrixer.save();
-    return { updatedPrixer, unableArts, unableServices };
+    const updatedPrixer = await toUpdatePrixer.save()
+    return { updatedPrixer, unableArts, unableServices }
     // }
   } catch (e) {
-    console.log(e);
-    return e;
+    console.log(e)
+    return e
   }
-};
+}
 
 const updateBio = async (prixerId, prixerData) => {
   try {
     const toUpdatePrixer = await Prixer.findByIdAndUpdate(prixerId, {
       bio: prixerData,
-    });
+    })
     if (!toUpdatePrixer) {
-      return console.log("Prixer update error: " + err);
+      return console.log("Prixer update error: " + err)
     } else
-      return { success: true, message: "Actualización realizada con éxito." };
+      return { success: true, message: "Actualización realizada con éxito." }
   } catch (e) {
-    console.log(e);
-    return e;
+    console.log(e)
+    return e
   }
-};
+}
 
 const updateTermsAgreeGeneral = async (prixerId, prixerData) => {
   try {
     const toUpdatePrixer = await Prixer.find({
       _id: prixerId,
-    });
-    toUpdatePrixer.termsAgree = prixerData;
+    })
+    toUpdatePrixer.termsAgree = prixerData
 
-    const updatedPrixer = await toUpdatePrixer.save();
+    const updatedPrixer = await toUpdatePrixer.save()
     if (!updatedPrixer) {
-      return console.log("Prixer update error: " + err);
+      return console.log("Prixer update error: " + err)
     }
-    return "Actualización realizada con éxito.";
+    return "Actualización realizada con éxito."
   } catch (e) {
-    console.log(e);
-    return e;
+    console.log(e)
+    return e
   }
-};
+}
 
 const updateTermsAgree = async (prixerId, prixerData) => {
   try {
     const toUpdatePrixer = await Prixer.findOne({
       username: prixerId,
-    });
+    })
 
-    toUpdatePrixer.termsAgree = prixerData.termsAgree;
-    const updatedPrixer = await toUpdatePrixer.save();
+    toUpdatePrixer.termsAgree = prixerData.termsAgree
+    const updatedPrixer = await toUpdatePrixer.save()
     if (!updatedPrixer) {
-      return console.log("Prixer update error: " + err);
+      return console.log("Prixer update error: " + err)
     } else {
-      return { success: true, message: "Actualización realizada con éxito." };
+      return { success: true, message: "Actualización realizada con éxito." }
     }
   } catch (e) {
-    console.log(e);
-    return e;
+    console.log(e)
+    return e
   }
-};
+}
 
 const addRole = async (req, res) => {
   try {
-    const updated = await User.updateMany({}, { role: "Prixer" });
-    res.send({ role: "Prixer", prixers: updated });
+    const updated = await User.updateMany({}, { role: "Prixer" })
+    res.send({ role: "Prixer", prixers: updated })
   } catch (err) {
-    console.log(err);
-    res.status(500).send(err);
+    console.log(err)
+    res.status(500).send(err)
   }
-};
+}
 
-const disablePrixer = (prixerData) => {};
+const disablePrixer = (prixerData) => {}
 
 const removePrixers = async () => {
-  const removedPrixers = await Prixer.deleteMany({});
+  const removedPrixers = await Prixer.deleteMany({})
   if (removedPrixers) {
-    return "Se eliminaron: " + removedPrixers;
+    return "Se eliminaron: " + removedPrixers
   } else {
-    return removedPrixers;
+    return removedPrixers
   }
-};
+}
+
+const destroyPrixer = async (prixerId, username) => {
+  const destroyUser = await User.findOneAndDelete({ username: username })
+  const destroyPrixer = await Prixer.findOneAndDelete({ _id: prixerId })
+  const destroyArts = await Art.deleteMany({ prixerUsername: username })
+  const destroyServices = await Service.deleteMany({ prixerUsername: username })
+  const destroyConsumer = await Consumer.deleteMany({ prixerId: prixerId })
+
+  if (destroyPrixer && destroyUser) {
+    return {
+      message: `Prixer eliminado. Usuario eliminado. ${destroyArts.deletedCount} Artes eliminados. ${destroyServices.deletedCount} Servicios eliminados. Clientes con el prixerID eliminado`,
+      user: destroyUser,
+      prixer: destroyPrixer,
+      arts: destroyArts,
+      services: destroyServices,
+      consumer: destroyConsumer
+    }
+  } else {
+    return {
+      message: "Algo salió mal, revisa los detalles.",
+      user: destroyUser,
+      prixer: destroyPrixer,
+      arts: destroyArts,
+      services: destroyServices,
+      consumer: destroyConsumer
+    }
+  }
+}
+
 //CRUD END
 
 module.exports = {
@@ -376,6 +408,7 @@ module.exports = {
   updateTermsAgree,
   disablePrixer,
   removePrixers,
+  destroyPrixer,
   readAllPrixersFull,
   addRole,
-};
+}
