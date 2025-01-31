afbc8a686b48ec7dcfd69d09285072abb658fe5b
Edward Guerrero <iamwar2070@gmail.com>
2025-01-31 00:57:57 -0400

    chore: updating data according to the refactor on frontend

--- admin/adminControllers/adminAuthControllers.js
+++ admin/adminControllers/adminAuthControllers.js
@@ -3,26 +3,29 @@ const adminAuthServices = require("../adminServices/adminAuthServices");
 const createAdmin = async (req, res) => {
   try {
     let checkPermissions = await adminAuthServices.checkPermissions(
-      req.body.adminToken
+      req.cookies.adminToken
     );
+
     if (checkPermissions.role.modifyAdmins) {
+
       const result = await adminAuthServices.createAdmin(req.body);
-      if (result.res.success) {
+
+      if (result?.success) {
         const adminToken = adminAuthServices.generateToken(result.newAdmin);
         if (adminToken) {
           const isProduction = process.env.NODE_ENV === "prod";
           var expiryDate = new Date(Date.now() + 2400 * 60 * 1000);
           result.adminToken = adminToken;
-          res
-            .cookie("adminToken", adminToken, {
-              secure: true,
-              httpOnly: true,
-              sameSite: "None",
-              domain: isProduction ? ".prixelart.com" : "localhost",
-              path: "/",
-              maxAge: expiryDate,
-            })
-            .send({ success: true });
+          // res
+          //   .cookie("adminToken", adminToken, {
+          //     secure: true,
+          //     httpOnly: true,
+          //     sameSite: "None",
+          //     domain: isProduction ? ".prixelart.com" : "localhost",
+          //     path: "/",
+          //     maxAge: expiryDate,
+          //   })
+            res.send({ success: true, newAdmin: result.newAdmin });
         }
       } else {
         res.send(result);
--- admin/adminServices/adminAuthServices.js
+++ admin/adminServices/adminAuthServices.js
@@ -1,23 +1,24 @@
-const Admin = require("../adminModel")
-const bcrypt = require("bcrypt")
-const jwt = require("jsonwebtoken")
-const adminServices = require("./adminServices")
-const dotenv = require("dotenv")
-const adminRoleModel = require("../adminRoleModel")
-dotenv.config()
+const Admin = require("../adminModel");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const adminServices = require("./adminServices");
+const dotenv = require("dotenv");
+const adminRoleModel = require("../adminRoleModel");
+dotenv.config();
 
 const createAdmin = async (adminData) => {
   try {
-    const readedAdminByEmail = await adminServices.readAdminByEmail(adminData)
+
+    const readedAdminByEmail = await adminServices.readAdminByEmail(adminData);
     const readedAdminByUsername = await adminServices.readAdminByUsername(
       adminData.username
-    )
+    );
     if (readedAdminByUsername) {
       return {
         success: false,
         info: "error_username",
         message: "Disculpa, el nombre de usuario ya está registrado.",
-      }
+      };
     }
 
     if (readedAdminByEmail) {
@@ -25,28 +26,27 @@ const createAdmin = async (adminData) => {
         success: false,
         info: "error_email",
         message: "Disculpa, el correo del usuario ya está registrado.",
-      }
+      };
     }
+    const salt = await bcrypt.genSalt(2);
+    const hash = await bcrypt.hash(adminData.password, salt);
 
-    const salt = await bcrypt.genSalt(2)
-    const hash = await bcrypt.hash(adminData.password, salt)
-
-    adminData.password = hash
-    let newAdmin = await new Admin(adminData).save()
+    adminData.password = hash;
+    let newAdmin = await new Admin(adminData).save();
     return {
-      res: { success: true, adminId: newAdmin._id },
+      success: true,
       newAdmin: newAdmin,
-    }
+    };
   } catch (e) {
-    return "Incapaz de crear el usuario, intenta de nuevo o consulta a soporte."
+    return "Incapaz de crear el usuario, intenta de nuevo o consulta a soporte.";
   }
-}
+};
 
 const authenticate = async (adminData) => {
   try {
     const admin = await adminServices.readFullAdminByEmail({
       email: adminData.email,
-    })
+    });
 
     if (admin) {
       if (!bcrypt.compareSync(adminData.password, admin.password)) {
@@ -56,12 +56,12 @@ const authenticate = async (adminData) => {
           adminId: null,
           error_info: "error_pw",
           error_message: "Inténtalo de nuevo, contraseña incorrecta.",
-        }
+        };
       } else {
         const permissions = await adminRoleModel
           .findOne({ area: admin.area })
           .select("-_id -area -__v")
-          .exec()
+          .exec();
 
         const payload = {
           username: admin.username,
@@ -73,11 +73,11 @@ const authenticate = async (adminData) => {
           permissions: permissions,
           id: admin._id,
           time: new Date(),
-        }
+        };
 
         const adminToken = jwt.sign(payload, process.env.ADMIN_JWT_SECRET, {
           expiresIn: process.env.ADMIN_TOKEN_EXPIRE_TIME,
-        })
+        });
 
         return {
           success: true,
@@ -86,7 +86,7 @@ const authenticate = async (adminData) => {
           adminId: admin._id,
           error_info: null,
           error_message: null,
-        }
+        };
       }
     } else {
       return {
@@ -96,13 +96,13 @@ const authenticate = async (adminData) => {
         error_info: "error_email",
         error_message:
           "No se encuentra el email, por favor solicita un usuario con acceso.",
-      }
+      };
     }
     // }
   } catch (e) {
-    console.log(e)
+    console.log(e);
   }
-}
+};
 
 const generateToken = (admin) => {
   try {
@@ -113,38 +113,36 @@ const generateToken = (admin) => {
       password: admin.password,
       id: admin._id,
       time: new Date(),
-    }
+    };
 
     const adminToken = jwt.sign(payload, process.env.ADMIN_JWT_SECRET, {
       expiresIn: process.env.ADMIN_TOKEN_EXPIRE_TIME,
-    })
+    });
 
-    return adminToken
+    return adminToken;
   } catch (err) {
-    resizeBy.status(400).json({ error: err })
+    resizeBy.status(400).json({ error: err });
   }
-}
+};
 
 const ensureAuthenticated = (req, res, next) => {
   try {
-    console.log("req.cookies", req.cookies);
-    const adminToken =
-      req.cookies.adminToken || undefined
+    const adminToken = req.cookies.adminToken || undefined;
     if (!adminToken) {
       return res.send({
         success: false,
         error_info: "auth error",
         error_message:
           "No has iniciado sesión. Por favor inicia sesión para continuar.",
-      })
+      });
     } else {
       jwt.verify(adminToken, process.env.JWT_SECRET, (err, decoded) => {
         if (err)
           return res
             .status(500)
-            .send({ auth: false, message: "Falló autenticación de token." })
-        next()
-      })
+            .send({ auth: false, message: "Falló autenticación de token." });
+        next();
+      });
     }
   } catch (e) {
     res.send({
@@ -152,54 +150,57 @@ const ensureAuthenticated = (req, res, next) => {
       error_info: "error",
       error_message:
         "No has iniciado sesión. Por favor inicia sesión para continuar.",
-    })
+    });
   }
-}
+};
 
 function verifyToken(token, secret) {
   return new Promise((resolve, reject) => {
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
       if (err) {
-        reject(err)
+        reject(err);
       } else {
-        resolve(decoded)
+        resolve(decoded);
       }
-    })
-  })
+    });
+  });
 }
 
 const checkPermissions = async (req, res) => {
   try {
-    let token
-    if (req.body?.adminToken !== undefined) {
-      token = req.body.adminToken
+    let token;
+    if (req?.cookies?.adminToken !== undefined) {
+      token = req?.cookies.adminToken;
       if (token) {
-        const decoded = await verifyToken(token)
+        const decoded = await verifyToken(token);
         if (decoded && decoded !== undefined) {
           let readedRole = await adminRoleModel.findOne({
             area: decoded.area,
-          })
-          await res.send({ readedRole })
+          });
+          await res.send({ readedRole });
         } else {
           return {
             auth: false,
             error: err,
             message: "Falló autenticación de token.",
-          }
+          };
         }
       }
     } else {
-      token = req
-      const decoded = await verifyToken(token)
+      token = req;
+      console.log(token)
+      const decoded = await verifyToken(token);
+      console.log("descifrado", decoded);
       if (decoded && decoded !== undefined) {
-        let readedRole = await adminRoleModel.findOne({ area: decoded.area })
-        return { admin: decoded, role: readedRole }
+        let readedRole = await adminRoleModel.findOne({ area: decoded.area });
+        console.log("rol", readedRole);
+        return { admin: decoded, role: readedRole };
       } else {
         return {
           auth: false,
           error: err,
           message: "Falló autenticación de token.",
-        }
+        };
       }
     }
   } catch (e) {
@@ -208,9 +209,9 @@ const checkPermissions = async (req, res) => {
       error_info: e,
       error_message:
         "No has iniciado sesión. Por favor inicia sesión para continuar.",
-    }
+    };
   }
-}
+};
 
 module.exports = {
   createAdmin,
@@ -218,4 +219,4 @@ module.exports = {
   generateToken,
   ensureAuthenticated,
   checkPermissions,
-}
+};
