688165342e54904cc9c7fcaca330abf2cdb70f10
Edward Guerrero <iamwar2070@gmail.com>
2025-05-04 23:01:34 -0400

    chore: trying to use mailGun

--- user/userControllers/userControllers.js
+++ user/userControllers/userControllers.js
@@ -1,7 +1,11 @@
-// const FormData = require("form-data"); // form-data v4.0.1
-// const Mailgun = require("mailgun.js");
-const userService = require("../userServices/userServices")
-
+const userService = require("../userServices/userServices");
+const formData = require("form-data");
+const Mailgun = require("mailgun.js");
+const mailgun = new Mailgun(formData);
+const mg = mailgun.client({
+  username: "api",
+  key: process.env.MAILGUN_API_KEY || "2021646eb484aae665e0d3b213425f1b",
+});
 //Es importante modificar userService por userServices.
 
 //CRUD
@@ -9,18 +13,18 @@ const userService = require("../userServices/userServices")
 
 async function readUserById(req, res) {
   try {
-    const readedUser = await userService.readUserById(req.user)
-    return res.send(readedUser)
+    const readedUser = await userService.readUserById(req.user);
+    return res.send(readedUser);
   } catch (err) {
-    res.status(500).send(err)
+    res.status(500).send(err);
   }
 }
 
 async function readUserByUsername(req, res) {
   try {
-    return await userService.readUserByUsername(req)
+    return await userService.readUserByUsername(req);
   } catch (err) {
-    res.status(500).send(err)
+    res.status(500).send(err);
   }
 }
 
@@ -32,61 +36,61 @@ async function updateUser(req, res) {
       lastName: req.body.lastName,
       email: req.body.email,
       login_count: req.login_count,
-    }
-    const updatedUser = await userService.updateUser(user)
-    return res.send(updatedUser)
+    };
+    const updatedUser = await userService.updateUser(user);
+    return res.send(updatedUser);
   } catch (err) {
-    res.status(500).send(err)
+    res.status(500).send(err);
   }
 }
 
 const readUserByAccount = async (req, res) => {
   try {
-    const account = req.body.account
-    const getUser = await userService.readUserByAccount(account)
-    return res.send(getUser)
+    const account = req.body.account;
+    const getUser = await userService.readUserByAccount(account);
+    return res.send(getUser);
   } catch (error) {
-    res.status(500).send(error)
+    res.status(500).send(error);
   }
-}
+};
 
 async function disableUser(req, res) {
   try {
-    const disabledUser = await userService.disableUser(req.body)
-    return res.send(disabledUser)
+    const disabledUser = await userService.disableUser(req.body);
+    return res.send(disabledUser);
   } catch (err) {
-    res.status(500).send(err)
+    res.status(500).send(err);
   }
 }
 
-// async function testEmails() {
-//   const mailgun = new Mailgun(FormData);
-//   const mg = mailgun.client({
-//     username: "api",
-//     key: process.env.MAILGUN_API_KEY|| "API_KEY",
-//     // When you have an EU-domain, you must specify the endpoint:
-//     // url: "https://api.eu.mailgun.net/v3"
-//   });
-//   try {
-//     const data = await mg.messages.create("sandbox2e58b581f9484b3ab04e21a4b110a37f.mailgun.org", {
-//       from: "Mailgun Sandbox <postmaster@sandbox2e58b581f9484b3ab04e21a4b110a37f.mailgun.org>",
-//       to: ["Edward <iamwar2070@gmail.com>"],
-//       subject: "Hello Edward",
-//       text: "Congratulations Edward, you just sent an email with Mailgun! You are truly awesome!",
-//     });
+const testEmails = async () => {
+  const mailgun = new Mailgun(FormData);
+  const mg = mailgun.client({
+    username: "api",
+    key: process.env.MAILGUN_API_KEY || "2021646eb484aae665e0d3b213425f1b",
+  });
+  try {
+    mg.messages
+      .create("sandbox2e58b581f9484b3ab04e21a4b110a37f.mailgun.org", {
+        from: "Mailgun Sandbox <postmaster@sandbox2e58b581f9484b3ab04e21a4b110a37f.mailgun.org>",
+        to: ["Edward <iamwar2070@gmail.com>"],
+        subject: "Hello Edward",
+        text: "Congratulations Edward, you just sent an email with Mailgun! You are truly awesome!",
+      })
+      .then((msg) => console.log(msg))
+      .catch((err) => console.error(err));
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-//     console.log(data); // logs response data
-//   } catch (error) {
-//     console.log(error); //logs any error
-//   }
-// }
 module.exports = {
   readUserById,
   readUserByUsername,
   readUserByAccount,
   updateUser,
   disableUser,
-  // testEmails
-}
+  testEmails,
+};
 
 //CRUD END
