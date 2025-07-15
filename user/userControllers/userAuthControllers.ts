58fdb88fc7bac7f68994f40eb054773e48ec3e12
Edward Guerrero <iamwar2070@gmail.com>
2025-07-15 15:54:31 -0400

    fix: reset prod vars

--- user/userControllers/userAuthControllers.ts
+++ user/userControllers/userAuthControllers.ts
@@ -50,8 +50,8 @@ export const login = async (req: Request, res: Response, next: NextFunction): Pr
     res.cookie("token", auth.result! as string, {
       secure: isProduction,
       httpOnly: true,
-      sameSite: "lax",
-      domain:  "localhost",
+      sameSite: isProduction ? "none" : "lax",
+      domain: isProduction ? ".prixelart.com" : "localhost",
       path: "/",
       maxAge: 240 * 60 * 1000,
     })
