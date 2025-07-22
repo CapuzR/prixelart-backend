241d2e8e289b78c881091edfc9b90c6b03cb1153
Edward Guerrero <iamwar2070@gmail.com>
2025-07-22 15:02:22 -0400

    feat: create the script, update Permissions model

--- admin/permissionsModel.ts
+++ admin/permissionsModel.ts
@@ -37,3 +37,115 @@ export interface Permissions {
   updateDollar: boolean
   updateTermsAndCo: boolean
 }
+
+export interface PermissionsV2 {
+  _id?: ObjectId
+  area: string
+  admins: {
+    createAdmin: boolean
+    createAdminRole: boolean
+    deleteAdmin: boolean
+    readAdmins: boolean
+    readAdminRoles: boolean
+    updateAdmin: boolean
+    updateAdminRole: boolean
+  }
+  art: {
+    artBan: boolean
+    createArt: boolean
+    readAllArts: boolean
+    updateArt: boolean
+    deleteArt: boolean
+  }
+  discounts: {
+    createDiscount: boolean
+    deleteDiscount: boolean
+    readAllDiscounts: boolean
+    updateDiscount: boolean
+    useDiscount: boolean
+  }
+  movements: {
+    createMovement: boolean
+    deleteMovement: boolean
+    reverseMovement: boolean
+    readAllMovements: boolean
+    readMovementsByPrixer: boolean
+    updateMovement: boolean
+  }
+  orders: {
+    archiveOrder: boolean
+    create: boolean
+    deleteOrder: boolean
+    downloadData: boolean
+    readAllOrders: boolean
+    readHistory: boolean
+    readOrderDetails: boolean
+    readPayDetails: boolean // read prices and vouchers register
+    updateDetails: boolean // basicInfo: client data, billing or shipping data
+    updateGeneralStatus: boolean
+    updateItem: boolean // after creation can change the Item, WARNING: this will change price, subtotal, taxs, total costs, etc
+    updateItemPrice: boolean
+    updateItemStatus: boolean
+    updatePayDetails: boolean
+    updatePayStatus: boolean
+    updateSeller: boolean
+  }
+  paymentMethods: {
+    createPaymentMethod: boolean
+    deletePaymentMethod: boolean
+    readAllPaymentMethod: boolean
+    updatePaymentMethod: boolean
+  }
+  preferences: {
+    createBanner: boolean
+    deleteBanner: boolean
+    readAllBanners: boolean
+    updateArtBestSellers: boolean
+    updateBanner: boolean
+    updateBestSellers: boolean
+    updateDollarValue: boolean
+    updateTermsAndCo: boolean
+  }
+  products: {
+    createProduct: boolean
+    createVariant: boolean
+    deleteProduct: boolean
+    deleteVariant: boolean
+    downloadData: boolean
+    loadData: boolean
+    readAllProducts: boolean
+    updateImages: boolean
+    updateMockup: boolean
+    updateProduct: boolean
+    updateVariant: boolean
+  }
+  shippingMethod: {
+    createShippingMethod: boolean
+    deleteShippingMethod: boolean
+    readAllShippingMethod: boolean
+    updateShippingMethod: boolean
+  }
+  surcharges: {
+    createSurcharge: boolean
+    deleteSurcharge: boolean
+    readAllSurcharges: boolean
+    updateSurcharge: boolean
+    useSurcharge: boolean
+  }
+  testimonials: {
+    createTestimonial: boolean
+    deleteTestimonial: boolean
+    readTestimonials: boolean
+    updateTestimonial: boolean
+  }
+  users: {
+    banConsumer: boolean
+    banPrixer: boolean
+    banUser: boolean
+    createConsumer: boolean
+    deleteConsumer: boolean
+    readAllUsers: boolean
+    readPrixerBalance: boolean
+    setPrixerBalance: boolean
+  }
+}
